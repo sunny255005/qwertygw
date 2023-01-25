98c2b442ae36d34fe954ac68b7320dd76708c65e
sunny255005 <107548368+sunny255005@users.noreply.github.com>
2023-01-25 11:46:11 +0530

    Add files via upload

---
+++ index.test.js
@@ -0,0 +1,34 @@
+//const app = require("../server");
+const app=require("../index")
+// const index = require('../index.js')
+ const supertest = require('supertest');
+// const request = supertest(index);
+
+
+//     describe("/health",()=>{
+
+//         test("Should respond a status code 200",async  ()=>{
+//             const response=await request.get("/health").send({
+                
+//             })
+//             expect(response.text).toBe("Ok")
+//             expect(response.statusCode).toBe(200)
+            
+//         })
+//     })
+
+test("GET /health", async () => {
+  //  const post = await Post.create({ title: "Post 1", content: "Lorem ipsum" });
+  
+    // await supertest(server).get("/health")
+    //   .expect(200)
+    
+                    const response=await supertest(app).get("/health").send({
+                        
+                    })
+                    expect(response.text).toBe("Ok")
+                    expect(response.statusCode).toBe(200)
+                    
+             
+      
+  });
